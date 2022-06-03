f29b1e57c56cfd005288972700dbd38e99b43b28
mikcl <mikesmikes400@gmail.com>
2022-06-03 10:53:41 +0100

    fixup

--- linear_attention_transformer/linear_attention_transformer.py
+++ linear_attention_transformer/linear_attention_transformer.py
@@ -546,7 +546,6 @@ class VitEmbedding(nn.Module):
 # github:lucidrains/vit-pytorch/vit_pytorch efficient.py simple_vit.py
 
 def posemb_sincos_2d(patches, temperature = 10000, dtype = torch.float32):
-    print("Patches.shape Begin - ", patches.shape, "device:", patches.device, "type:", patches.dtype)
     _, h, w, dim, device, dtype = *patches.shape, patches.device, patches.dtype
 
     y, x = torch.meshgrid(torch.arange(h, device = device), torch.arange(w, device = device), indexing = 'ij')
@@ -557,7 +556,6 @@ def posemb_sincos_2d(patches, temperature = 10000, dtype = torch.float32):
     y = y.flatten()[:, None] * omega[None, :]
     x = x.flatten()[:, None] * omega[None, :] 
     pe = torch.cat((x.sin(), x.cos(), y.sin(), y.cos()), dim = 1)
-    print("Patches.shape End - ", pe.shape)
 
     return pe.type(dtype)
 
